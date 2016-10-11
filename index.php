9e8577fcf2b111909733594a3abb5371af2f088e
Kevin Dilts <kdilts13@gmail.com>
2016-10-11 16:37:15 -0600

    added a style sheet

--- index.php
+++ index.php
@@ -3,8 +3,15 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>About Me</title>
+		<link href="stylesheet.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
-
+		<header></header>
+		<hr/>
+		<main>
+			<section></section>
+			<hr/>
+			<section></section>
+		</main>
 	</body>
 </html>
